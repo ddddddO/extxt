f6878b1c74072d616d8ea799c4fba0e77903a636
ddddddO <lbfdeatq@gmail.com>
2020-12-31 02:30:12 +0900

    Modify html

--- server.go
+++ server.go
@@ -23,13 +23,23 @@ const indexHTML = `
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Extxt</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <style type="text/css">
+        #main {
+            margin: 33vh auto 0;
+            transform: translateY(-50%);
+            text-align: center;
+        }
+    </style>
+    <title>Extxt</title>
 </head>
 <body>
-	<form action="/" method="post" enctype="multipart/form-data">
-		<input type="file" name="src_file">
-		<button type="submit">text!</button>
-	</form>
+    <div id="main">
+        <form action="/" method="post" enctype="multipart/form-data">
+            <input type="file" name="src_file" required>
+            <button type="submit">text!</button>
+        </form>
+    </div>
 </body>
 </html>
 `
